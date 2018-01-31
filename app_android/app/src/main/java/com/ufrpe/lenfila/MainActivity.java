4ab90720eaa1ffa3f1c81229f7c4b4e2099445c2
Filipe Mei <peticormei@gmail.com>
2018-01-31 10:13:53 -0300

    Requisição GET funcinando com JSON

--- app_android/app/src/main/java/com/ufrpe/lenfila/MainActivity.java
+++ app_android/app/src/main/java/com/ufrpe/lenfila/MainActivity.java
@@ -5,18 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     TextView textView;
-    String serverUrl = "http://192.168.31.139:5000";
+    String serverUrl = "http://172.16.204.174:5000/api/tamanho";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +29,29 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button)findViewById(R.id.bn);
         textView = (TextView)findViewById(R.id.txt);
+        textView.setText(serverUrl);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, serverUrl,
-                        new Response.Listener<String>() {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, serverUrl,
+                        new Response.Listener<JSONObject>() {
                             @Override
-                            public void onResponse(String response) {
-                                textView.setText(response);
+                            public void onResponse(JSONObject response) {
+                                String resposta = response.toString();
+                                Toast.makeText(MainActivity.this, resposta, Toast.LENGTH_LONG).show();
                                 requestQueue.stop();
                             }
                         }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        textView.setText("Something went wrong...");
+                        String resposta = error.toString();
+                        Toast.makeText(MainActivity.this, resposta, Toast.LENGTH_LONG).show();
                         error.printStackTrace();
                         requestQueue.stop();
                     }
                 });
-                requestQueue.add(stringRequest);
+                requestQueue.add(jsonObjectRequest);
             }
         });
     }
